a0bfa1bdb5c5805359ca33507acf6b97e4553f7a
nicmorais <45640048+nicmorais@users.noreply.github.com>
2019-02-13 20:11:57 -0200

    Update fceux.sh

--- Installers/fceux.sh
+++ Installers/fceux.sh
@@ -9,10 +9,10 @@ if test -f ~/.pocket-home/.version; then
   IS_ICON_PRESENT=`jq '.pages[0] | .items[] | select(.name == "FCEUX")' ~/.pocket-home/config.json`
   if [ -z ${IS_ICON_PRESENT} ]
   then
-    jq '(.pages[0] | .items) |= . + [{ "name": "FCEUX", "icon": "/usr/local/bin/pocketinstaller/Icons/fceux.png", "shell": "fceux" }]' ~/.pocket-home/config.json > tmp.$$.json
+    jq '(.pages[0] | .items) |= . + [{ "name": "FCEUX", "icon": "/usr/local/bin/pocketinstaller/Icons/fceux.png", "shell": "vala-terminal -e fceux && exit" }]' ~/.pocket-home/config.json > tmp.$$.json
     mv tmp.$$.json ~/.pocket-home/config.json
   fi
 fi
 echo -e '\nTo enter Fullscreen, go to Options -> Fullscreen.'
-echo "To exit the emulator, press ALT+Enter, then CTRL+Q"
+echo "Then, to exit the emulator, press ALT+Enter, and then CTRL+Q"
 
