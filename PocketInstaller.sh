a460d432126e1a32f85c7757b29d319ce2cd2a66
nicmorais <45640048+nicmorais@users.noreply.github.com>
2019-02-13 19:11:31 -0200

    add fceux option

--- PocketInstaller.sh
+++ PocketInstaller.sh
@@ -151,9 +151,13 @@ if test -f ~/.pocket-home/.version; then
   :
 else
   P20="PocketHome(Marshmallow)|Installers/pockethome.sh"
+if hash fceux 2>/dev/null; then
+  :
+else
+  P21="FCEUX(NES)|Installers/fceux.sh"
 fi
 
-menu=($P1 $P2 $P3 $P4 $P5 $P6 $P7 $P8 $P9 $P10 $P11 $P12 $P13 $P14 $P15 $P16 $P17 $P18 $P19 $P20)
+menu=($P1 $P2 $P3 $P4 $P5 $P6 $P7 $P8 $P9 $P10 $P11 $P12 $P13 $P14 $P15 $P16 $P17 $P18 $P19 $P20 $P21)
 
 yad_opts=(--form
 --scroll
