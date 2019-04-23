026ca7fe98a8cc5e262d9301967d61ff10c43fa9
nicmorais <45640048+nicmorais@users.noreply.github.com>
2019-04-23 16:29:03 -0300

    Update cavestory.sh

--- Installers/cavestory.sh
+++ Installers/cavestory.sh
@@ -2,15 +2,9 @@
 echo "Installing Cave Story"
 sudo apt-get install libsdl-ttf2.0-dev build-essential -y
 cd ~
-git clone https://github.com/EXL/NXEngine/
-git clone https://github.com/libretro/nxengine-libretro nxengine-libretro
-# move cavestory from nxengine-libretro to NXEngine
-mv nxengine-libretro/datafiles/Doukutsu.exe NXEngine/
-mv nxengine-libretro/datafiles/data/* NXEngine/data
-# define resolution
-sed -i 's/# -D_L10N_CP1251/-D_480X272/' NXEngine/Makefile.linux
+git clone https://github.com/nicmorais/Pocket-NXEngine NXEngine
 cd NXEngine
-make -f Makefile.linux
+make
 
 if test -f ~/.pocket-home/.version; then
   IS_ICON_PRESENT=`jq '.pages[0] | .items[] | select(.name == "Cave Story")' ~/.pocket-home/config.json`
@@ -23,7 +17,10 @@ fi
 
 echo
 echo "Cave Story Installed. Have fun!"
-sleep 3
+echo "Controls: k,l,n,m"
+echo "Left, Right, Up and Down"
+echo "If you are running XFCE, cd to ~/NXEngine and run ./nx"
+sleep 5
 killall pocket-home && nohup pocket-home &
 kill -9 $PPID
 
